4d9703018a6777cfa6e4d3d6603a1a07cc3403f0
Charles Howard <frodare@gmail.com>
2017-05-11 23:19:57 -0400

    More GUI working

--- java/net/torocraft/minecoprocessors/gui/GuiMinecoprocessor.java
+++ java/net/torocraft/minecoprocessors/gui/GuiMinecoprocessor.java
@@ -8,6 +8,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.torocraft.minecoprocessors.Minecoprocessors;
 import net.torocraft.minecoprocessors.blocks.ContainerMinecoprocessor;
+import net.torocraft.minecoprocessors.processor.Register;
 
 // TODO loading arrow support
 
@@ -25,13 +26,16 @@ import net.torocraft.minecoprocessors.blocks.ContainerMinecoprocessor;
 
 public class GuiMinecoprocessor extends net.minecraft.client.gui.inventory.GuiContainer {
 
-  private static final ResourceLocation BREWING_STAND_GUI_TEXTURES =
-      new ResourceLocation(Minecoprocessors.MODID, "textures/gui/minecoprocessor.png");
+  private static final ResourceLocation BREWING_STAND_GUI_TEXTURES = new ResourceLocation(Minecoprocessors.MODID, "textures/gui/minecoprocessor.png");
 
   private final IInventory playerInventory;
   private final IInventory minecoprocessor;
 
+  private byte[] registers = {(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff};
+
   private GuiButton buttonReset;
+  private GuiButton buttonPause;
+  private GuiButton buttonStep;
 
   public GuiMinecoprocessor(IInventory playerInv, IInventory p_i45506_2_) {
     super(new ContainerMinecoprocessor(playerInv, p_i45506_2_));
@@ -39,53 +43,93 @@ public class GuiMinecoprocessor extends net.minecraft.client.gui.inventory.GuiCo
     this.minecoprocessor = p_i45506_2_;
   }
 
-
   /**
    * Draw the foreground layer for the GuiContainer (everything in front of the items)
    */
   protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-
 
     GlStateManager.pushMatrix();
     GlStateManager.scale(0.5d, 0.5d, 0.5d);
     int scale = 2;
     int x = 130 * scale;
     int y = 20 * scale;
-    fontRendererObj.drawString("A  B  C  D ", x, y - 5, 0x404040);
-    fontRendererObj.drawString("0F 01 00 00", x, y + 10, 0xffffff);
+    // fontRendererObj.drawString("A B C D ", x, y - 5, 0x404040);
 
-    //PF
-    centered(scale, "00", 88, 23);
-    
-    //PR
-    centered(scale, "01", 108, 43);
-    
-    //PL
-    centered(scale, "02", 68, 43);
-    
-    //PB
-    centered(scale, "03", 88, 63);
+    // fontRendererObj.drawString("0F 01 00 00", x, y + 10, 0xffffff);
 
+    y = 50;
+    drawRegister(Register.A, 130 * 2, y);
+    drawRegister(Register.B, 139 * 2, y);
+    drawRegister(Register.C, 148 * 2, y);
+    drawRegister(Register.D, 157 * 2, y);
 
+    y = 82;
+    drawFlag("Z", true, 130 * 2, y);
+    drawFlag("C", false, 139 * 2, y);
+    drawFlag("F", false, 148 * 2, y);
+    drawFlag("S", true, 157 * 2, y);
+
+    // PF
+    centered("00", 176, 47);
+
+    // PR
+    centered("01", 216, 86);
+
+    // PL
+    centered("02", 137, 86);
+
+    // PB
+    centered("03", 176, 125);
+
+    drawCode();
 
     GlStateManager.popMatrix();
 
     drawGuiTitle();
     drawInventoryTitle();
+
+    drawButtons();
+
   }
 
+  private void drawCode() {
+    int x = 22;
+    int y = 50;
 
-  private void centered(int scale, String s, float x, float y) {
-    int xs = (int) x * scale - fontRendererObj.getStringWidth(s) / 2;
-    int ys = (int) y * scale - fontRendererObj.FONT_HEIGHT / 2;
+    String label = "NEXT 0F";
+    String value = "MOV A, B";
+
+    fontRendererObj.drawString(label, x - 4, y - 14, 0x404040);
+    fontRendererObj.drawString(value, x, y, 0xffffff);
+  }
+
+  private void drawRegister(Register register, int x, int y) {
+    String label = register.toString();
+    String value = Integer.toHexString(registers[register.ordinal()]).substring(0, 2);
+    drawLabeledValue(label, value, x, y);
+  }
+
+  private void drawFlag(String label, boolean flag, int x, int y) {
+    String value = flag ? "1" : "0";
+    drawLabeledValue(label, value, x, y);
+  }
+
+  private void drawLabeledValue(String label, String value, int x, int y) {
+    int wLabel = fontRendererObj.getStringWidth(label) / 2;
+    int wValue = fontRendererObj.getStringWidth(value) / 2;
+
+    fontRendererObj.drawString(label, x - wLabel, y - 14, 0x404040);
+    fontRendererObj.drawString(value, x - wValue, y, 0xffffff);
+  }
+
+  private void centered(String s, float x, float y) {
+    int xs = (int) x - fontRendererObj.getStringWidth(s) / 2;
+    int ys = (int) y - fontRendererObj.FONT_HEIGHT / 2;
     fontRendererObj.drawString(s, xs, ys, 0xffffff);
   }
 
-
-
   private void drawInventoryTitle() {
-    fontRendererObj.drawString(playerInventory.getDisplayName().getUnformattedText(), 8,
-        ySize - 96 + 2, 4210752);
+    fontRendererObj.drawString(playerInventory.getDisplayName().getUnformattedText(), 8, ySize - 96 + 2, 4210752);
   }
 
   private void drawGuiTitle() {
@@ -96,18 +140,39 @@ public class GuiMinecoprocessor extends net.minecraft.client.gui.inventory.GuiCo
   @Override
   public void initGui() {
     super.initGui();
+
+  }
+
+  private void drawButtons() {
     int buttonId = 0;
-    buttonReset = new GuiButton(buttonId++, 10, 10, I18n.format("reset", (Object) null));
+    int x = 8 + guiLeft;
+    int y = 34 + guiTop;
+    int width = 49;
+    int height = 10;
+
+    buttonReset = new ScaledGuiButton(buttonId++, x, y, width, height, I18n.format("gui.button.reset", (Object) null));
+    buttonPause = new ScaledGuiButton(buttonId++, x, y + 11, width, height, I18n.format("gui.button.pause", (Object) null));
+    buttonStep = new ScaledGuiButton(buttonId++, x, y + 22, width, height, I18n.format("gui.button.step", (Object) null));
+    
+    buttonStep.enabled = false;
+
     buttonList.add(buttonReset);
+    buttonList.add(buttonStep);
+    buttonList.add(buttonPause);
   }
 
   @Override
   protected void actionPerformed(GuiButton button) {
     if (button == buttonReset) {
-      System.out.println("reset button pressed");
+      System.out.println("Reset Button Pressed");
+    }
+    if (button == buttonPause) {
+      System.out.println("Pause Button Pressed");
+    }
+    if (button == buttonStep) {
+      System.out.println("Step Button Pressed");
     }
   }
-
 
   /**
    * Draws the background layer of this container (behind the items).
